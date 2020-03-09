cf0bd533c91dedd15f830d20a026578fef269f6f
ildar <ildarets@mail.ru>
2020-03-09 18:08:43 +0300

    Не работают ссылки

---
+++ avto/avtoapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from avtoapp import views
+
+app_name = 'avtoapp'
+
+urlpatterns = [
+    path('', views.main_view, name = 'index'),
+    path('create/', views.create_post, name = 'create')
+]
--- avto/avtoapp/views.py
+++ avto/avtoapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Marks, Mesto, Avto
+# Create your views here
 
-# Create your views here.
+avto_name = 'avtoapp'
+
+def main_view(request):
+    marks = Marks.objects.all()
+    mesto = Mesto.objects.all()
+    avto = Avto.objects.all()
+
+    return render(request,'avtoapp/index.html', context={'marks': marks,
+                                                         'mesto': mesto,
+                                                         'avto': avto})
+
+def create_post(request):
+    return render(request, 'avtoapp/create.html')
