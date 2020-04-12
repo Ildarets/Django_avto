845700537a41519352d5ef1fbca842d33899e59f
ildar <ildarets@mail.ru>
2020-04-12 17:05:56 +0300

    Восстановил, Все работает! Сделал тесты

--- avto/avtoapp/tests.py
+++ avto/avtoapp/tests.py
@@ -5,13 +5,28 @@ from faker import Faker
 from mixer.backend.django import mixer
 
 
-class PostTestCaseMixer(TestCase):
+class AvtoTestCaseMixer(TestCase):
 
     def setUp(self):
         self.post = mixer.blend(Avto)
 
 
     def test_has_image(self):
-        self.assertFalse(self.post.has_image())
+        self.assertFalse(self.post.has_price())
 
 
+class MarksTestMixer(TestCase):
+    def setUp(self):
+        self.marka = Marks.objects.create(name = 'test_name')
+
+    def test_str(self):
+        self.assertEqual(str(self.marka), 'test_name')
+    #
+
+
+class MestoTestMixer(TestCase):
+    def setUp(self):
+        self.marka = Mesto.objects.create(name='test_name_mesto')
+
+    def test_str(self):
+        self.assertEqual(str(self.marka), 'test_name_mesto')
