1dcf49d369bc6a5daed3b6445d3a6cc763dd9a93
ildar <ildarets@mail.ru>
2020-05-20 00:03:32 +0300

    Сделал token

--- avto/api_request.py
+++ avto/api_request.py
@@ -2,6 +2,10 @@ import requests
 import pprint
 
 
-response = requests.get('http://127.0.0.1:8000/mesto/avto/')
+# response = requests.get('http://127.0.0.1:8000/api/v0/mesto/', auth = ('Ildarets', '9205ildar'))
+
+token = 'df56a36ffb2af5566313e245ed75320ef01ee9cd'
+headers = {'Authorization': f'Token {token}'}
+response = requests.get('http://127.0.0.1:8000/api/v0/mesto/', headers = headers)
 
 pprint.pprint(response.json())
--- avto/avtoapp/api_views.py
+++ avto/avtoapp/api_views.py
@@ -1,15 +1,22 @@
 from .models import Avto, Marks, Mesto
 from .serializers import AvtoSerializer, MarksSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from .permissions import ReadOnly, IsAuthor
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication, TokenAuthentication
 
 class AvtoViewSet(viewsets.ModelViewSet):
-    queryset = Avto.objects.select_related('cat_marka', 'cat_mesto').all()
+    permission_classes = [IsAdminUser|ReadOnly]
+    queryset = Avto.objects.select_related('cat_marka', 'cat_mesto')
     serializer_class = AvtoSerializer
 
 class MarksViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthor|ReadOnly]
     queryset = Marks.objects.all()
     serializer_class = MarksSerializer
 
 class MestoViewSet(viewsets.ModelViewSet):
+    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Mesto.objects.all()
     serializer_class = MarksSerializer
